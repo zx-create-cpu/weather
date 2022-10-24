a37e42dd7397aa26c1d41395ca9e2cdbf5e3b04a
marrtyer <101202923+marrtyer@users.noreply.github.com>
2022-10-24 10:59:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -218,7 +218,7 @@ def send_message(to_user, access_token, region_name, weather, tempMAX, tempMIN, 
         # 获取距离下次生日的时间
         birth_day = get_birthday(value["birthday"], year, today)
         if birth_day == 0:
-            birthday_data = "今天{}生日哦，祝{}生日快乐！".format(value["name"], value["name"])
+            birthday_data = "今天是{}的生日哦，祝{}生日快乐！".format(value["name"], value["name"])
         else:
             birthday_data = "距离{}的生日还有{}天".format(value["name"], birth_day)
         # 将生日数据插入data
