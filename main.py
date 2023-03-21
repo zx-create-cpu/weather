bc71465a48ee385bacf5057543304d491e3e525b
marrtyer <101202923+marrtyer@users.noreply.github.com>
2023-03-21 09:18:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def get_weather(region):
         os.system("pause")
         sys.exit(1)
     elif response["code"] == "401":
-        print("推送消息失败，请检查和风天气key是否正确！")
+        print("推送消息失败，请检查和风天气key是否正确")
         os.system("pause")
         sys.exit(1)
     else:
